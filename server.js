37d34869795d6020ea36fe2dfe7df502162fedfd
GaoCarri <gaocarri@163.com>
2019-11-23 15:00:04 +0800

    简单的动态服务器

---
+++ server.js
@@ -0,0 +1,124 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if (!port) {
+    console.log('请指定端口号ok不ok？\nnode server.js 8888 这样不会吗？')
+    process.exit(1)
+}
+
+var server = http.createServer(function(request, response) {
+    var parsedUrl = url.parse(request.url, true)
+    var pathWithQuery = request.url
+    var queryString = ''
+    if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+    var path = parsedUrl.pathname
+    var query = parsedUrl.query
+    var method = request.method
+
+    /******** 从这里开始看，上面不要看 ************/
+    const session = JSON.parse(fs.readFileSync('./session.json').toString())
+    console.log('有个东西访问了' + pathWithQuery)
+    if (path === '/sign_in' && method === 'POST') {
+
+        response.setHeader('Content-Type', 'text/html;charset=UTF-8');
+        const usersArray = JSON.parse(fs.readFileSync('db/user.json'));
+        const array = [];
+        request.on('data', chunk => {
+            array.push(chunk)
+        });
+        request.on('end', () => {
+            const string = Buffer.concat(array).toString();
+            const obj = JSON.parse(string) //数据库里有name 和password
+            const user = usersArray.find((user) => user.name === obj.name && user.password === obj.password)
+            if (user === undefined) {
+                response.statusCode = 400
+                response.end(`{"errorCode":531}`)
+            } else {
+                response.statusCode = 200;
+                const random = Math.random()
+                session[random] = { user_id: user.id }
+                fs.writeFileSync('session.json', JSON.stringify(session))
+                response.setHeader("Set-Cookie", `session_id=${random}; HttpOnly`);
+
+            }
+            response.end();
+        })
+    } else if (path === '/home.html') {
+        const cookie = request.headers['cookie']
+        let sessionId;
+        try {
+            sessionId = cookie
+                .split(";")
+                .filter(s => s.indexOf("session_id=") >= 0)[0]
+                .split('=')[1];
+        } catch (error) {}
+        if (sessionId) {
+            const userId = session[sessionId].user_id
+            const usersArray = JSON.parse(fs.readFileSync('db/user.json'));
+            const user = usersArray.find(user => user.id === userId);
+            const homeHTML = fs.readFileSync('public/home.html').toString();
+            let string = ''
+            if (user) {
+                string = homeHTML.replace('{{loginStatus}}', "已登录")
+                    .replace('{{user.name}}', user.name)
+            }
+            response.write(string);
+            response.end()
+        } else {
+            const homeHTML = fs.readFileSync('public/home.html').toString()
+            const string = homeHTML.replace('{{loginStatus}}', '未登录')
+            response.write(string)
+            response.end()
+        }
+
+    } else if (path === '/register' && method === 'POST') {
+        response.setHeader('Content-Type', 'text/html;charset=UTF-8');
+        const usersArray = JSON.parse(fs.readFileSync('db/user.json'));
+        const array = [];
+        request.on('data', (chunk) => {
+            array.push(chunk)
+        });
+        request.on('end', () => {
+            const string = Buffer.concat(array).toString();
+            const obj = JSON.parse(string)
+            const lastUser = usersArray[usersArray.length - 1]
+            const newUser = {
+                id: lastUser ? lastUser.id + 1 : 1,
+                name: obj.name,
+                password: obj.password
+            }
+            usersArray.push(newUser);
+            fs.writeFileSync('db/user.json', JSON.stringify(usersArray))
+            response.end()
+        })
+    } else {
+        response.statusCode = 200;
+        let filePath = path === '/' ? '/index.html' : path; //默认首页
+        const index = filePath.lastIndexOf('.');
+        const suffix = filePath.substring(index); //suffix是后缀的意思
+        const fileTypes = {
+            '.html': 'text/html',
+            '.css': 'text/css',
+            '.js': 'text/javascript',
+            '.png': 'image/png',
+            '.jpg': 'image/jpeg'
+        }
+        response.setHeader('Content-Type', `${fileTypes[suffix] || 'text/html'};charset=utf-8`);
+        let content;
+        try {
+            content = fs.readFileSync(`./public${filePath}`)
+        } catch (error) {
+            content = '文件不存在'
+            response.statusCode = 404
+        }
+
+        response.write(content);
+        response.end()
+    }
+    /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\nCtrl + C 打开 http://localhost:' + port)
